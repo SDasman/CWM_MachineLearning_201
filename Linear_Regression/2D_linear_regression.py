ad8f3da68e00d19407bc8722e23019de795e9bc9
Shivakumar Mahakali <mahakalim@yahoo.com>
2017-11-28 19:36:30 -0500

    Added comments and extra formulas for 2D and polynomial regression scripts.

--- Linear_Regression/2D_linear_regression.py
+++ Linear_Regression/2D_linear_regression.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-# load in the data
+# load in the data from 'data_2d.csv'
 X = []
 Y = []
 
@@ -11,22 +11,29 @@ for line in open('data_2d.csv'):
     x1, x2, y = line.split(',')
     X.append([float(x1), float(x2), 1])     # This is our bias w_0, x_0 which equals 1 term.
     Y.append(float(y))
-
-# convert X and Y into numpy arrays:
+    # We add a bis term to "move / adjust" our line of best fit.
+# convert X and Y into Numpy arrays:
 X = np.array(X)
 Y = np.array(Y)
 
-# plot just to see what it looks like:
+# Plot to see what our data looks like:
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(X[:, 0], X[:, 1], Y)
 plt.show()
 
+# For multi-dimensional regression we have D equations and D unknowns. We want to isolate w to find the weights.
+# Our model still takes the form (Y_hat = wT * Xi) and our error function does not change.
+# Take derivative of error function and solving for w:
+
+# w(XT X) = XT Y (this takes the form Ax = b, solving for w:
+# w = ((XT X)^-1) XT Y convert into Numpy methods:
+
 # calculate the weights for our data:
 w = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, Y))
 Y_hat = np.dot(X, w)
 
-# compute R^2 to see how good out model is:
+# compute R2 to determine how good our model is:
 d1 = Y - Y_hat
 d2 = Y - Y.mean()
 R2 = 1 - d1.dot(d1) / d2.dot(d2)
--- Linear_Regression/polynomial_linear_regression.py
+++ Linear_Regression/polynomial_linear_regression.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# load in the data
+# load in the data from 'data_poly.csv'
 X = []
 Y = []
 
@@ -12,19 +12,27 @@ for line in open('data_poly.csv'):
     X.append([1, x, x*x])
     Y.append(float(y))
 
-# convert to numpy arrays
+# convert to Numpy arrays
 X = np.array(X)
 Y = np.array(Y)
 
-# calculate weights:
+# For multi-dimensional regression we have D equations and D unknowns. We want to isolate w to find the weights.
+# Our model still takes the form (Y_hat = wT * Xi) and our error function does not change.
+# Take derivative of error function and solving for w:
+
+# w(XT X) = XT Y (this takes the form Ax = b, solving for w:
+# w = ((XT X)^-1) XT Y convert into Numpy methods:
+
+# calculate the weights for our data:
 w = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, Y))
 Y_hat = np.dot(X, w)
 
+# Plot to see the data and line of best fit.
 plt.scatter(X[:, 1], Y)
-plt.plot(sorted(X[:, 1]), sorted(Y_hat))
+plt.plot(sorted(X[:, 1]), sorted(Y_hat), color='black')
 plt.show()
 
-# calculate R^2
+# Calculate R2 to determine how good our model is:
 d1 = Y - Y_hat
 d2 = Y - Y.mean()
 R2 = 1 - d1.dot(d1) / d2.dot(d2)
